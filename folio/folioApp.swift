265d536c70fa4170a24234b805b56eac4c90e433
TheLifeIsYours <tliyours@gmail.com>
2021-10-29 22:33:53 +0200

    Initial Commit

---
+++ folio/folioApp.swift
@@ -0,0 +1,17 @@
+//
+//  folioApp.swift
+//  folio
+//
+//  Created by Mats Daniel Larsen on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct folioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
